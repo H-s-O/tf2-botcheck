cb6cadc922783427d751e10e985be8fcbe50a16b
H-s-O <hugostonge@gmail.com>
2021-01-01 17:09:11 -0500

    get team of players

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const BOT_LIST = require('./data/bots.json');
 const HASH = Date.now().toString(36);
 const START_MARKER = `-bc.${HASH}-`;
 const END_MARKER = `-/bc.${HASH}-`;
-const STATUS_LINE_REGEXP = /^#\s+(\d+)\s+"(.+)"/gm;
+const NAME_LINE_REGEXP = /^"name" = "(.+?)"/g;
+const LOBBY_LINE_REGEXP = /^  Member\[\d+\] \[(U:.+?)\]  team = (\w+)/gm;
+const STATUS_LINE_REGEXP = /^#\s+(\d+)\s+"(.+?)"\s+\[(U:.+?)\]/gm;
 const BOT_CHECK_REGEXP_TEMPLATE = (name) => RegExp(`^(\\(\\d+\\))*${name}$`);
 const CONSOLE_KEY = '/';
 
@@ -18,7 +20,7 @@ robot.setKeyboardDelay(0);
 console.info('Sending TF2 console keystrokes');
 robot.typeString(CONSOLE_KEY);
 sleep.msleep(10);
-sendkeys.sync(`echo ${START_MARKER};status;`);
+sendkeys.sync(`echo ${START_MARKER};name;tf_lobby_debug;status;`);
 sleep.msleep(10);
 robot.keyTap('enter');
 sleep.msleep(250);
@@ -29,18 +31,18 @@ sleep.msleep(10);
 robot.keyTap('escape');
 
 // Read log file
-console.info('Opening TF2 log file');
+console.info('Reading TF2 console log file');
 const logPath = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Team Fortress 2\\tf\\console.log';
 const logContent = fs.readFileSync(logPath, { encoding: 'utf8' });
 
 // Find last occurence of start and end markers
-const startMarkerPos = logContent.indexOf(START_MARKER);
+const startMarkerPos = logContent.lastIndexOf(START_MARKER);
 if (startMarkerPos === -1) {
     // Start marker not found, abort
     console.error('Could not find start marker, aborting');
     process.exit(1);
 }
-const endMarkerPos = logContent.indexOf(END_MARKER);
+const endMarkerPos = logContent.lastIndexOf(END_MARKER);
 if (endMarkerPos === -1) {
     // End marker not found, abort
     console.error('Could not find end, aborting');
@@ -59,11 +61,13 @@ console.info(statusContent);
 
 // Parsing players list
 const players = [];
-let matches;
-while ((matches = STATUS_LINE_REGEXP.exec(statusContent)) !== null) {
+let playerMatches;
+while ((playerMatches = STATUS_LINE_REGEXP.exec(statusContent)) !== null) {
     players.push({
-        userid: matches[1],
-        name: matches[2].replace(/[^\u0020-\u007E\u00A0-\u02AD]/g, ''), // remove invisible characters possibly added by hijacking bots
+        userid: playerMatches[1],
+        name: playerMatches[2],
+        cleanName: playerMatches[2].replace(/[^\u0020-\u007E\u00A0-\u02AD]/g, ''), // remove invisible characters possibly added by hijacking bots
+        uniqueid: playerMatches[3],
     });
 }
 if (players.length === 0) {
@@ -72,12 +76,43 @@ if (players.length === 0) {
     process.exit(0);
 }
 
+// Parsing lobby debug info
+const lobby = [];
+let lobbyMatches;
+while ((lobbyMatches = LOBBY_LINE_REGEXP.exec(statusContent)) !== null) {
+    lobby.push({
+        uniqueid: lobbyMatches[1],
+        team: lobbyMatches[2],
+    });
+}
+
+if (lobby.length !== players.length) {
+    // Mismatch between players status count and lobby debug info count, abort
+    console.error('Status and lobby debug mismatch, aborting');
+    process.exit(1);
+}
+
+// Merge lobby debug info into player info
+for (const player of players) {
+    for (const info of lobby) {
+        if (info.uniqueid === player.uniqueid) {
+            player.team = info.team;
+        }
+    }
+}
+
+// Parsing current player name
+const nameMatches = NAME_LINE_REGEXP.exec(statusContent);
+const currentPlayerName = nameMatches[1];
+const currentPlayerInfo = players.find(({ name }) => name === currentPlayerName);
+console.info('Current player name:', currentPlayerName, 'uniqueid:', currentPlayerInfo.uniqueid, 'team:', currentPlayerInfo.team);
+
 // Loop each bot name and check against each player name
 const foundBots = [];
 for (const botDefinition of BOT_LIST) {
     for (const player of players) {
         const botRegExp = BOT_CHECK_REGEXP_TEMPLATE(botDefinition.name);
-        if (botRegExp.test(player.name)) {
+        if (botRegExp.test(player.cleanName)) {
             console.info('Found bot:', player.name);
             foundBots.push(player);
         }
@@ -86,10 +121,10 @@ for (const botDefinition of BOT_LIST) {
 // Loop the player list and check for duplicates
 const playerCounts = {};
 for (const player of players) {
-    if (!(player.name in playerCounts)) {
-        playerCounts[player.name] = 0;
+    if (!(player.cleanName in playerCounts)) {
+        playerCounts[player.cleanName] = 0;
     }
-    playerCounts[player.name]++;
+    playerCounts[player.cleanName]++;
 }
 const foundDuplicates = Object.entries(playerCounts)
     .filter(([, count]) => count > 1)
