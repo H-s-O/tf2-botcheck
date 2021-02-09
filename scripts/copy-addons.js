a9d2661f626f6e408edbe304f91d95cd1c41cb4f
H-s-O <hugostonge@gmail.com>
2021-02-08 20:28:13 -0500

    use nexe to create portable executable

---
+++ scripts/copy-addons.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const cpy = require('cpy');
+
+const modulesDir = path.join(__dirname, '..', 'node_modules')
+const binaries = path.join('**', '*.node');
+const buildDir = path.join(__dirname, '..', 'build', 'node_modules');
+cpy(binaries, buildDir, { cwd: modulesDir, parents: true });
