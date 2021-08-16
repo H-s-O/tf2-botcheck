1a6766d2c22fa68ac5fef5755a12eb44ae8d0e48
H-s-O <hugostonge@gmail.com>
2021-08-16 18:14:54 -0400

    add new ranges to excluded characters list

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ while ((playerMatches = STATUS_LINE_REGEXP.exec(statusContent)) !== null) {
     players.push({
         userid: playerMatches[1],
         name: playerMatches[2],
-        cleanName: playerMatches[2].replace(/[\u0000-\u001F\u007F-\u009F\u200B-\u200F\u2028-\u202E\u2066-\u206F]/g, ''), // remove invisible characters possibly added by hijacking bots
+        cleanName: playerMatches[2].replace(/[\u0000-\u001F\u007F-\u009F\u200B-\u200F\u2028-\u202E\u2066-\u206F\u0300-\u036F\u0E31-\u0ECD]/g, ''), // remove invisible characters possibly added by hijacking bots
         uniqueid: playerMatches[3],
         connected: PARSE_CONNECTED_TIME(playerMatches[4]),
         ping: parseInt(playerMatches[5]),
